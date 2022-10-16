685879cad0fbada95591149293086d7696492156
Toriqul Islam Leon <yeitoriqul@gmail.com>
2022-10-16 09:50:56 +0600

    Header added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="text-center"></div>;
+  return (
+    <div className="text-white overflow-hidden">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
