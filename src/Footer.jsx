e34d7ba9e8e22fd93ad8de02bb756e09a3e7299c
Toriqul Islam Leon <yeitoriqul@gmail.com>
2022-10-21 14:02:15 +0600

    Animation added on Hero

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,22 +3,23 @@ import CenterMenu from "./components/CenterMenu";
 import { Facebook, Twitter, YouTube, LinkedIn } from "@material-ui/icons";
 
 function Footer() {
+  const SocialStyle = "rounded-full border-2 border-whit p-2 mr-[5rem]";
   return (
     <div className="footer flex flex-col items-center justify-start px-[5rem] bg-[#081730] h-[45rem] pt-[18rem] mt-[-10rem] relative z-[-1]">
       <CenterMenu></CenterMenu>
 
       {/* Social Icon */}
       <div className="flex w-[100%] justify-center mt-14 ">
-        <div className="facebook">
+        <div className={SocialStyle}>
           <Facebook></Facebook>
         </div>
-        <div className="twitter">
+        <div className={SocialStyle}>
           <Twitter></Twitter>
         </div>
-        <div className="youtube">
+        <div className={SocialStyle}>
           <YouTube></YouTube>
         </div>
-        <div className="linkedin">
+        <div className={SocialStyle}>
           <LinkedIn></LinkedIn>
         </div>
       </div>
--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -59,8 +59,8 @@ function MusicPlayer() {
                   y2="1"
                   gradientUnits="objectBoundingBox"
                 >
-                  <stop offset="0" stop-color="#f2061d" />
-                  <stop offset="1" stop-color="#e600fe" />
+                  <stop offset="0" stopColor="#f2061d" />
+                  <stop offset="1" stopColor="#e600fe" />
                 </linearGradient>
               </defs>
               <path
@@ -87,8 +87,8 @@ function MusicPlayer() {
                   y2="1.074"
                   gradientUnits="objectBoundingBox"
                 >
-                  <stop offset="0" stop-color="#f2061e" />
-                  <stop offset="1" stop-color="#e600fd" />
+                  <stop offset="0" stopColor="#f2061e" />
+                  <stop offset="1" stopColor="#e600fd" />
                 </linearGradient>
               </defs>
               <rect
