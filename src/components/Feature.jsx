dd4e777f27bf419b3ed224af562cdda75c906f39
Toriqul Islam Leon <yeitoriqul@gmail.com>
2022-10-24 08:50:22 +0600

    Done

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,23 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
+import VisibilitySensor from "react-visibility-sensor";
+import { motion } from "framer-motion";
 const Feature = (props) => {
+  const variant = {
+    true: {
+      transform: "scale(1)",
+    },
+    false: {
+      transform: "scale(0.5)",
+    },
+  };
+
+  const [elementIsVisible, setElementIsVisible] = useState(false);
+
   return (
-    <div className=" flex flex-col items-center justify-center relative mx-[12rem] text-center">
-      <div className="icon bg-[#081730] rounded-2xl p-4">
-        <img
-          src={require(`../img/${props.icon}.png`)}
-          alt=""
-          className="w-[3rem]"
-        />
+    <VisibilitySensor onChange={(isVisible) => setElementIsVisible(isVisible)}>
+      <div className=" flex flex-col items-center justify-center relative mx-[12rem] text-center">
+        <motion.div
+          variants={variant}
+          transition={{
+            duration: 1,
+            type: "ease-out",
+          }}
+          animate={`${elementIsVisible}`}
+          className="icon bg-[#081730] rounded-2xl p-4"
+        >
+          <img
+            src={require(`../img/${props.icon}.png`)}
+            alt=""
+            className="w-[3rem]"
+          />
+        </motion.div>
+        <span className="mt-5">{props.title}</span>
+        <span className="text-[#707070] mt-4">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
+        </span>
+        <span className="text-[#e600ff] underline mt-[3rem] hover:cursor-pointer">
+          Learn More
+        </span>
       </div>
-      <span className="mt-5">{props.title}</span>
-      <span className="text-[#707070] mt-4">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis
-      </span>
-      <span className="text-[#e600ff] underline mt-[3rem] hover:cursor-pointer">
-        Learn More
-      </span>
-    </div>
+    </VisibilitySensor>
   );
 };
 
